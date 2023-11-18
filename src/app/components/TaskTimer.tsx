45052d978d9e02c3b34aa506f51d615dd8d83bcb
yuya-0928 <hirako.yuuya0928.4620@gmail.com>
2023-11-19 02:57:33 +0900

    タスクのスタートボタンを押したら、タイマーの開始とタスク名を表示するようにする

--- src/app/components/TaskTimer.tsx
+++ src/app/components/TaskTimer.tsx
@@ -1,8 +1,10 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TaskTimerContext } from "../context/TaskTimerContextType";
+import getTask from "../services/indexedDB/getTask";
 
 const TaskTimer = () => {
-  const {isRunning, setIsRunning, setCurrentTaskId, elapsedTime, setElapsedTime} = useContext(TaskTimerContext);
+  const {isRunning, setIsRunning, currentTaskId, setCurrentTaskId, elapsedTime, setElapsedTime} = useContext(TaskTimerContext);
+  const [currentTaskName, setCurrentTaskName] = useState<string>("");
 
   useEffect(() => {
     if(isRunning) {
@@ -11,11 +13,29 @@ const TaskTimer = () => {
       }, 1000);
       return () => clearInterval(interval);
     }
-  }, [isRunning, setElapsedTime])
+  }, [isRunning, setElapsedTime]);
+
+  useEffect(() => {
+    if(currentTaskId) {
+      getTaskInfo(currentTaskId);
+    }
+  }, [currentTaskId]);
 
   const handlePause = () => {
     setCurrentTaskId(null);
     setIsRunning(false);
+  }
+
+  const getTaskInfo = (taskId: number) => {
+    console.log("getTaskInfo");
+    (async () => {
+      try {
+        const task = await getTask(taskId);
+        setCurrentTaskName(task.taskName);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
   }
 
   const seconds = `0${Math.floor(elapsedTime / 1000) % 60}`.slice(-2);
@@ -25,6 +45,8 @@ const TaskTimer = () => {
   return (
     <div>
       <h1>Task Timer</h1>
+      <p>TaskId: {currentTaskId}</p>
+      <p>TaskName: {currentTaskName}</p>
       <p>{hours}:{minutes}:{seconds}</p>
       {isRunning && (<button onClick={handlePause}>Pause</button>)}
     </div>
